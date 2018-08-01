bbc7c6affa9cd601c25a5c5877f6a0ab868cc54d
QAJamesClive <Jamesclive@academytrainee.com>
2018-08-01 17:15:19 +0100

    Next commit

--- accountsApplication/src/code/Service.java
+++ accountsApplication/src/code/Service.java
@@ -20,4 +20,15 @@ public class Service {
 	public JSONObject displayHashMap() {
 		return new JSONObject(hMapAccounts);
 	}
+	
+	public int countSameNameAccounts(String firstName,String lastName) {
+		int count =0;
+		for(Map.Entry<Integer, Account> accounts : hMapAccounts.entrySet()){
+			if(((retrieveAccount(accounts.getKey()).getFirstName()).toString().equals(firstName))&&((retrieveAccount(accounts.getKey()).getLastName()).toString().equals(lastName))) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
 }
